de77ce44b4d62fa13e27948eacc6d9385bb987b0
Hoodahashem <mahmoudhashem335@gmail.com>
2024-03-29 10:49:37 +0200

    PassGuard Is Finished For FileBase And SQL-lite!

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,12 @@
 from os import getenv
-
-storage_type = getenv("PASSGUARD_STORAGE_TYPE")
-
-if storage_type == "db":
-    from models.Store_engine.DB_Storage import DB_Storage
-    storage = DB_Storage()
-else:
-    from models.Store_engine.File_Storge import File_Storage
-    storage = File_Storage()
+try:
+	storage_type = getenv("PASSGUARD_STORAGE_TYPE")
+	if storage_type == "db":
+		from models.Store_engine.DB_Storage import DB_Storage
+		storage = DB_Storage()
+	else:
+		from models.Store_engine.File_Storge import File_Storage
+		storage = File_Storage()
+except:
+	from models.Store_engine.File_Storge import File_Storage
+	storage = File_Storage()
