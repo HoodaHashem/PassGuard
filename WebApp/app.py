f162d8d90805c791baf52501c6fcd79091ffa2c3
Hoodahashem <mahmoudhashem335@gmail.com>
2024-03-31 05:28:39 +0200

    update!

--- WebApp/app.py
+++ WebApp/app.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html' , footer=True)
 
+@app.route('/Getintouch')
+def Getintouch():
+    return render_template('Getintouch.html' , footer=False, title='Get in touch')
 
 if __name__ == '__main__':
     app.run(debug=True)
